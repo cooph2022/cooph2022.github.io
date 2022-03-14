d5bcc80f572c4af65a989bdf321d4d09507c44c2
Coop <cooph2022@gmail.com>
2022-03-13 20:05:11 -0400

    Pearstone_test2

---
+++ potree-develop/pointclouds/Pearlstone_Converted_vold/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 29474241,
+    "boundingBox": {
+        "lx": -8555794.877869448,
+        "ly": 4795367.763623681,
+        "lz": 141.7326242649562,
+        "ux": -8554349.082869448,
+        "uy": 4796813.558623681,
+        "uz": 1587.5276242648818
+    },
+    "tightBoundingBox": {
+        "lx": -8555794.877869448,
+        "ly": 4795367.763623681,
+        "lz": 141.73262426495618,
+        "ux": -8554349.082869448,
+        "uy": 4796763.111623681,
+        "uz": 202.53262426495619
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 12.520952224731446,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
